05b6ba6103bd38da73eb1d665256aa123e290067
seyounggg <131657776+seyounggg@users.noreply.github.com>
2024-04-03 01:12:30 +0900

    분수의 덧셈.java

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         
-    int tmp1 = denom1 * denom2;
-    int tmp2 = numer1 * denom2;
+    int tmp1 = denom1 * denom2; // 공통분모 구하기
+    int tmp2 = numer1 * denom2; 
     int tmp3 = numer2 * denom1;
-    int tmp4 = tmp2 + tmp3;
+    int tmp4 = tmp2 + tmp3; // 분자의 합
 
     int c = method1(tmp4, tmp1);
 
@@ -12,6 +12,7 @@ class Solution {
     return answer;
   }
 
+  // 유클리드 호제법(최대공약수)
   public int method1(int a, int b){
       while(b != 0){
           int tmp = b;
