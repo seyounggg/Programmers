ef507bbbe4ccbb427a489aeda36f5d765c4688d2
seyounggg <131657776+seyounggg@users.noreply.github.com>
2024-05-08 22:31:52 +0900

    [level 0] Title: 모스부호 (1), Time: 0.12 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String letter) {
+        String[] m = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] s = letter.split(" ");
+        char[] answer = new char[s.length];
+
+        for(int i = 0; i < s.length; i++){
+            for(int j = 0; j < m.length; j++){
+                if(m[j].equals(s[i])){
+                    answer[i] = (char) (j+'a');
+                    break;
+                }
+            }
+        }
+
+        return new String(answer);
+    }
+}
