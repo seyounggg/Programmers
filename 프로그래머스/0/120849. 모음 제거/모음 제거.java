c61f1f992f029efb194533e77c128ca2619961d4
seyounggg <131657776+seyounggg@users.noreply.github.com>
2024-05-19 23:30:10 +0900

    [level 0] Title: 모음 제거, Time: 0.17 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,15 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        char[] strings = my_string.toCharArray();
-        StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < strings.length; i++){
-            if(strings[i] == 'a' || strings[i] == 'e' || strings[i] == 'i' || strings[i] == 'o' || strings[i] == 'u'){
-                sb.append("");
-                continue;
-            }
-            sb.append(strings[i]);
-        }
-        return sb.toString();
+        return my_string.replaceAll("[a,e,i,o,u]", "");
     }
 }
