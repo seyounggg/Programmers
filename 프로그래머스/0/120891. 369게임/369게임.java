db9a42876289717827e4f424e3c5933a56bf7ec8
seyounggg <131657776+seyounggg@users.noreply.github.com>
2024-05-22 21:43:48 +0900

    [level 0] Title: 369게임, Time: 3.19 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,15 @@
+import java.util.stream.Stream;
+
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        int[] array = Stream.of(String.valueOf(order).split("")).mapToInt(Integer::parseInt).toArray();
+        
+        for(int i : array){
+            if(i == 3 || i == 6 || i ==9){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
