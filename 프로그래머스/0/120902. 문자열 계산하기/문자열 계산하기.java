c3f0935c1eb8c7445a51b82503a6021241d4045e
seyounggg <131657776+seyounggg@users.noreply.github.com>
2024-05-28 23:53:07 +0900

    [level 0] Title: 문자열 계산하기, Time: 1.55 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,15 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(String my_string) {
-        String[] temp = my_string.split(" ");
-        int answer = Integer.parseInt(temp[0]);
         
-        for(int i = 1; i < temp.length; i+=2){
-            if(temp[i].equals("+")){
-                answer += Integer.parseInt(temp[i+1]);
-            } else {
-                answer -= Integer.parseInt(temp[i+1]);
-            }
-        }
-        return answer;
+        return Arrays.stream(my_string.replaceAll("- ","-").replaceAll("[+] ","")
+                            .trim().split(" ")).mapToInt(Integer::parseInt).sum();
     }
 }
