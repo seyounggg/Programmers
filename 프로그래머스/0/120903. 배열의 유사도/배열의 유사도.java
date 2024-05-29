865d46dcf30d1bdf3a32029200af7fb300044446
seyounggg <131657776+seyounggg@users.noreply.github.com>
2024-05-29 17:28:08 +0900

    [level 0] Title: 배열의 유사도, Time: 0.75 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        
+        return (int)Arrays.stream(s1).filter(s -> Arrays.asList(s2).contains(s)).count();
+    }
+}
