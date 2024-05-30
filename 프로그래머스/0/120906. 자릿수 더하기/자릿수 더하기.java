a53b1c6ccf6dc493eada7b456d36edacd7ac4cc1
seyounggg <131657776+seyounggg@users.noreply.github.com>
2024-05-30 22:49:54 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String temp = String.valueOf(n);
+        
+        for(int i = 0; i < temp.length(); i++){
+            answer += temp.charAt(i) - '0';
+        }
+        return answer;
+    }
+}
